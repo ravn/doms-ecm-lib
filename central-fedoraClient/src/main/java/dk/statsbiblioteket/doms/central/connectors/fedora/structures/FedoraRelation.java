d415d0fb63f39a4094a26ecb8e9bfe1c4aab8bb4
Asger Askov Blekinge <abr@statsbiblioteket.dk>
2015-03-25 12:59:03 +0100

    Doc of FedoraRelation

--- central-fedoraClient/src/main/java/dk/statsbiblioteket/doms/central/connectors/fedora/structures/FedoraRelation.java
+++ central-fedoraClient/src/main/java/dk/statsbiblioteket/doms/central/connectors/fedora/structures/FedoraRelation.java
@@ -3,8 +3,10 @@ package dk.statsbiblioteket.doms.central.connectors.fedora.structures;
 import dk.statsbiblioteket.doms.central.connectors.Connector;
 
 /**
- * Created by IntelliJ IDEA. User: abr Date: 3/15/11 Time: 10:25 AM To change this template use File | Settings | File
- * Templates.
+ * This object represent a relation, defined in the subject, with the predicate and "pointing" to the object. The object
+ * can be a literal value, but in that case, the literal boolean must be set. Subject and predicate must not be null.
+ *
+ * Subject and, if not literal, object, shall be URIs, meaning that they should start with info:fedora/
  */
 public class FedoraRelation {
     private String subject;
